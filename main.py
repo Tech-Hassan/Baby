1570d4368a32e1c4ee1a4971ba16e8f9aec1535a
Tech-Hassan <121412485+Tech-Hassan@users.noreply.github.com>
2026-06-30 11:46:07 +0500

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import uvicorn
-import os
-
-from app import app
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    uvicorn.run("app:app", host="0.0.0.0", port=port, log_level="info")
