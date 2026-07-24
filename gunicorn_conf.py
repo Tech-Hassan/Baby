ca754d009272caccaf9ff2454f4fdd3eadba9bc4
Tech-Hassan <121412485+Tech-Hassan@users.noreply.github.com>
2026-07-24 18:24:01 +0500

    Update gunicorn_conf.py

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,7 +1,9 @@
 import multiprocessing
+import os
 
+port = os.environ.get("PORT", "5002")
+bind = f"0.0.0.0:{port}"
 workers = min(multiprocessing.cpu_count() * 2 + 1, 8)
-bind = "0.0.0.0:$PORT"
 worker_class = "uvicorn.workers.UvicornWorker"
 timeout = 300
 graceful_timeout = 30
